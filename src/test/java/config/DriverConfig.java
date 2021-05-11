341285de49c7c53fc9c3034eb77d73123adcb402
espereverzeva <katya_novikova_1990_29@mail.ru>
2021-05-11 20:32:01 +0700

    Добавлены тесты.

--- src/test/java/config/DriverConfig.java
+++ src/test/java/config/DriverConfig.java
@@ -1,2 +1,12 @@
-package config;public interface DriverConfig {
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({ "classpath:config/driver.properties" })
+public interface DriverConfig extends Config {
+    @Key("remote.web.user")
+    String remoteWebUser();
+
+    @Key("remote.web.password")
+    String remoteWebPassword();
 }
--- src/test/java/tests/MainPageTests.java
+++ src/test/java/tests/MainPageTests.java
@@ -1,2 +1,71 @@
-package tests;public class MainPageTests {
+package tests;
+
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+@Story("Main page tests")
+public class MainPageTests extends TestBase {
+
+    @Test
+    @DisplayName("Проверяем первую страницу")
+    public void mainPageTests() {
+        step("Открываем главную страницу", () -> open("https://www.diasoft.ru/"));
+        step("Проверяем наличие логотипа", () -> {
+            $("img[src='/local/templates/diasoft_2019/images/logo_new.svg']").shouldBe(visible);
+        });
+
+    }
+
+    @Test
+    @DisplayName("Переход в раздел О Компании")
+    public void goToSection() {
+        step("Открываем главную страницу", () -> open("https://www.diasoft.ru/"));
+        step("Проверяем переход в раздел О Компании", () -> {
+            $(withText("О компании")).click();
+        });
+    }
+
+    @Test
+    @DisplayName("Переход на страницу с EN")
+    public void languageTranslation() {
+        step("Открываем главную страницу", () -> open("https://www.diasoft.ru/"));
+        step("Ищем переход на страницу с английским языком и кликаем на него", () -> {
+            $(".lang").shouldBe(visible).click();
+        });
+    }
+
+    @Test
+    @DisplayName("Поиск по сайту")
+    public void search() {
+        step("Открываем главную страницу", () -> open("https://www.diasoft.ru/"));
+        step("Ищем поисковое поле", () -> {
+            $(".js-img-search").shouldBe(visible).click();
+        });
+        step("Задаём поиск", () -> {
+            $("#qplSKIW").setValue("Контакты").pressEnter();
+        });
+        step("Проверяем результаты", () -> {
+            $(".breadcrumb-navigation").shouldHave(text("Поиск"));
+        });
+    }
+
+    @Test
+    @DisplayName("Переход ко всем новостям компании")
+    public void allCompanyNews() {
+        step("Открываем главную страницу", () -> open("https://www.diasoft.ru/"));
+        step("Ищем ссылку на переход ко всем новостям", () -> {
+            $(".all-news").shouldBe(visible).click();
+        });
+        step("Проверяем перехода к новостям", () -> {
+            $(".breadcrumb-navigation").shouldHave(text("Новости"));
+        });
+    }
 }
+
